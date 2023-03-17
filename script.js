cb883cc7e5439039830b78ea0ef6997ca6f937ac
Gabriel Rojas <gabriel.johann.rojas369@gmail.com>
2023-03-17 18:54:54 -0300

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// document.getElementById("caja").addEventListener("click", function() {
+//     document.getElementById("caja").style.display = "none";
+//     document.getElementById("mensaje").style.display = "block";
+// });
+
+let tapa = document.getElementById("tapa");
+let regalo = document.getElementById("regalo");
+let mandarina = document.getElementById("mandarina");
+let mensaje = document.getElementById("mensaje");
+tapa.addEventListener("click", function() {
+    tapa.classList.add("animation-tapa");
+    regalo.classList.add("animation-regalo");
+    mandarina.classList.add("animation-mandarina");
+
+    setTimeout(function() {
+        mensaje.classList.add("texto")
+    }, 500);
+
+    setTimeout(function() {
+        tapa.classList.add("achicar-arriba");
+        regalo.classList.add("achicar-abajo");
+    }, 2000);
+});
+
+regalo.addEventListener("click", function() {
+    regalo.classList.add("animation-regalo");
+    tapa.classList.add("animation-tapa");
+});
+
+//sonido
+document.getElementById("tapa").addEventListener("click", function() {
+	document.getElementById("sonidito").play();
+});
