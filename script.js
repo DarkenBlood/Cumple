560581523212f0d129f1ca979932fe7eee20d62f
Gabriel Rojas <gabriel.johann.rojas369@gmail.com>
2023-03-17 19:14:16 -0300

    mi tercer commit

--- script.js
+++ script.js
@@ -2,27 +2,12 @@
 //     document.getElementById("caja").style.display = "none";
 //     document.getElementById("mensaje").style.display = "block";
 // });
-
+let todo = document.getElementById("todo");
 let tapa = document.getElementById("tapa");
 let regalo = document.getElementById("regalo");
 let mandarina = document.getElementById("mandarina");
 let mensaje = document.getElementById("mensaje");
-tapa.addEventListener("click", function() {
-    tapa.classList.add("animation-tapa");
-    regalo.classList.add("animation-regalo");
-    mandarina.classList.add("animation-mandarina");
-
-    setTimeout(function() {
-        mensaje.classList.add("texto")
-    }, 500);
-
-    setTimeout(function() {
-        tapa.classList.add("achicar-arriba");
-        regalo.classList.add("achicar-abajo");
-    }, 2000);
-});
-
-regalo.addEventListener("click", function() {
+todo.addEventListener("click", function() {
     tapa.classList.add("animation-tapa");
     regalo.classList.add("animation-regalo");
     mandarina.classList.add("animation-mandarina");
@@ -38,10 +23,6 @@ regalo.addEventListener("click", function() {
 });
 
 //sonido
-document.getElementById("regalo").addEventListener("click", function() {
-	document.getElementById("sonidito").play();
-});
-
-document.getElementById("tapa").addEventListener("click", function() {
+document.getElementById("todo").addEventListener("click", function() {
 	document.getElementById("sonidito").play();
 });
